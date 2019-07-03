a6641a6bb285c6f007d6608d65f2494ea3f680a1
alif33 <alifhasan332@gmail.com>
2019-07-03 09:05:54 +0600

    .

--- admin/inc/sidebar.php
+++ admin/inc/sidebar.php
@@ -4,9 +4,9 @@
                     <ul class="section menu">
 						<li><a class="menuitem">Site Option</a>
                             <ul class="submenu">
-                                <li><a href="titleslogan.html">Title & Slogan</a></li>
-                                <li><a href="social.html">Social Media</a></li>
-                                <li><a href="copyright.html">Copyright</a></li>                                
+                                <li><a href="titleslogan.php">Title & Slogan</a></li>
+                                <li><a href="social.php">Social Media</a></li>
+                                <li><a href="copyright.php">Copyright</a></li>                                
                             </ul>
                         </li>
 						
@@ -18,14 +18,14 @@
                         </li>
                         <li><a class="menuitem">Category Option</a>
                             <ul class="submenu">
-                                 <li><a href="addcat.html">Add Category</a> </li>
-                                <li><a href="catlist.html">Category List</a> </li>
+                                 <li><a href="addcat.php">Add Category</a> </li>
+                                <li><a href="catlist.php">Category List</a> </li>
                             </ul>
                         </li>
                         <li><a class="menuitem">Post Option</a>
                             <ul class="submenu">
-                                <li><a href="addpost.html">Add Post</a> </li>
-                                <li><a href="postlist.html">Post List</a> </li>
+                                <li><a href="addpost.php">Add Post</a> </li>
+                                <li><a href="postlist.php">Post List</a> </li>
                             </ul>
                         </li>
                     </ul>
